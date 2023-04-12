7c8a8d78f4f61920b8e2ba0e915763fd3eab1985
UMBY2407 <umby2407@gmail.com>
2023-04-12 14:07:32 +0200

    Acquired book info

--- BookStoreCSharp/Program.cs
+++ BookStoreCSharp/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Acquisire le informazioni generiche del libro
+
+String bookName = "Clean Code";
+String authorName = "Robert C. Martin";
+String codiceIsbn13 = "978-0132350884";
+int pageNumber = 431;
+int bookWeight = 800;
+float bookWidth = 17.78f;
+float bookLenght = 23.37f;
+float bookHeight = 2.79f;
+
